5a849b3b02f3de53dc27fbafc240922d48304fea
Tia Krofel <tia.krofel@gmail.com>
2021-10-23 20:56:59 +0200

    Dodani vzorci in orodja

---
+++ podatki.py
@@ -0,0 +1,30 @@
+import re
+import requests
+import csv
+import os
+
+vzorec_bloka = re.compile(
+    r'<div class="content-card-text">'
+    r'(.*?)</been-there-everywhere>',
+    flags=re.DOTALL
+)
+
+vzorec_destinacije = re.compile(
+    r'href="(?P<url_konec>.*?)">.*?'
+    r'place-card-location">(?P<lokacija>.*?)</div>.*?'
+    r'js-title-content">(?P<destinacija>.*?)</span>.*?'
+    r'js-subtitle-content">(?P<opis>.*?)</div>.*?'
+    r'aria-hidden="true">.*?(?P<zem_sirina>-?\d+\.\d+),.*?'
+    r'\s(?P<zem_dolzina>-?\d+\.\d+).*?'
+    r'data-place-city="(?P<mesto>.*?)".*?'
+    r'data-place-country="(?P<drzava>.*?)".*?'
+    r'data-place-id=(?P<id>\d+?)\s',
+    flags=re.DOTALL
+)
+
+vzorec_znotraj = re.compile(
+    r'"DDPage__header-dek".*?'
+    r'title-md item-action-count">(?P<obiskali>\d+?)</div>'
+    r'title-md item-action-count">(?P<zelijo_obiskati>\d+?)</div>',
+    flags=re.DOTALL
+)
